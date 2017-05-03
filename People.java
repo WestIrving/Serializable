cba64d8ca4ed6f625c105d545c4bbaef398248fa
WestIrving <273933614@qq.com>
2017-05-03 22:11:16 +0800

    测试对象序列化

---
+++ People.java
@@ -0,0 +1,28 @@
+package 对象序列化;
+
+import java.io.Serializable;
+
+public class People implements Serializable{
+	private int id;
+	private String name;
+	private String sex;
+	
+	
+	public People(int id, String name, String sex) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.sex = sex;
+	}
+
+
+	@Override
+	public String toString() {
+		return "People [id=" + id + ", name=" + name + ", sex=" + sex + "]";
+	}
+	
+	
+	
+	
+
+}
---
+++ TestPeople.java
@@ -0,0 +1,52 @@
+package 对象序列化;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+public class TestPeople {
+	
+	public static void main(String[] args) {
+		
+		String file="C:\\Users\\Administrator.SKY-20170306IYS\\Desktop\\obj.dat";
+		
+		try {
+			//将对象存入文件中
+//			ObjectOutputStream oos=new ObjectOutputStream(new FileOutputStream(file));
+//			
+//			People p1=new People(1001, "张三", "男");
+//			People p2=new People(1002, "李四", "男");
+//			People p3=new People(1003, "王五", "男");
+			ArrayList list=new ArrayList();
+//			
+//			list.add(p1);
+//			list.add(p2);
+//			list.add(p3);
+//			oos.writeObject(list); //将对象写入File中
+//			
+//			oos.close();
+			
+			//将对象从文件中读出
+			ObjectInputStream ois=new ObjectInputStream(new FileInputStream(file));
+			
+			
+			list=(ArrayList)ois.readObject();
+			
+			System.out.println(list);
+			ois.close();
+			
+			
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+	}
+
+}
